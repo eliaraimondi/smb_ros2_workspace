c957fd586c1aeb2e2ea37adcf0d6d4c493181481
JaquelineSayuri <jaqsayuri@hotmail.com>
2025-06-24 18:45:14 +0000

    Fix node name

--- src/exploration_manager/src/exploration_manager.cpp
+++ src/exploration_manager/src/exploration_manager.cpp
@@ -2,19 +2,19 @@
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <std_msgs/msg/bool.hpp>
 
-class WaypointPublisher : public rclcpp::Node
+class ExplorationManager : public rclcpp::Node
 {
 public:
-    WaypointPublisher() : Node("waypoint_publisher")
+    ExplorationManager() : Node("exploration_manager")
     {
         // Create publisher for /way_point topic
-        waypoint_publisher_ = this->create_publisher<geometry_msgs::msg::PointStamped>(
+        exploration_manager_ = this->create_publisher<geometry_msgs::msg::PointStamped>(
             "/way_point", 10);
         
         // Create subscriber for /mission_complete topic
         mission_complete_subscriber_ = this->create_subscription<std_msgs::msg::Bool>(
             "/mission_complete", 10,
-            std::bind(&WaypointPublisher::mission_complete_callback, this, std::placeholders::_1));
+            std::bind(&ExplorationManager::mission_complete_callback, this, std::placeholders::_1));
         
         RCLCPP_INFO(this->get_logger(), "Waypoint Publisher Node started");
         RCLCPP_INFO(this->get_logger(), "Listening for mission_complete messages...");
@@ -40,7 +40,7 @@ private:
             waypoint_msg.point.z = 0.0;
             
             // Publish the message
-            waypoint_publisher_->publish(waypoint_msg);
+            exploration_manager_->publish(waypoint_msg);
             
             RCLCPP_INFO(this->get_logger(), "Published waypoint: (%.1f, %.1f, %.1f) in frame '%s'",
                        waypoint_msg.point.x, waypoint_msg.point.y, waypoint_msg.point.z,
@@ -52,7 +52,7 @@ private:
         }
     }
     
-    rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr waypoint_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr exploration_manager_;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr mission_complete_subscriber_;
 };
 
@@ -60,7 +60,7 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     
-    auto node = std::make_shared<WaypointPublisher>();
+    auto node = std::make_shared<ExplorationManager>();
     
     RCLCPP_INFO(node->get_logger(), "Spinning waypoint publisher node...");
     rclcpp::spin(node);
